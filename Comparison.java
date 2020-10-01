5194929e54d49fddd85dc4cb5b0b8314a94063db
Faremi Saheed <43324623+faremi@users.noreply.github.com>
2020-10-01 10:08:59 +0000

    Update Comparism.java

--- Comparison.java
+++ Comparison.java
@@ -6,12 +6,12 @@ public class Comparison {
 		
 		Scanner input = new Scanner(System.in);
 		
-		int num1, 
+		int number1, 
 		num2, 
 		num3;
 		
 		System.out.println("\nInput the first number: ");
-		num1 = input.nextInt();
+		number1 = input.nextInt();
 		
 		System.out.println("\nInput the second number: ");
 		num2 = input.nextInt();
@@ -19,27 +19,27 @@ public class Comparison {
 		System.out.println("\nInput the third number: ");
 		num3 = input.nextInt();
 	
-		if (num1 > num2 && num1 > num3) {
-			System.out.printf("\nThe first number %d is the largest of the three.\n",  num1);
+		if (number1 > num2 && number1 > num3) {
+			System.out.printf("\nThe first number %d is the largest of the three.\n",  number1);
 		}
 
-		else if (num2 > num1 && num2 > num3) {
+		else if (num2 > number1 && num2 > num3) {
 			System.out.printf("\nThe second number %d is the largest of the three.\n", num2);
 		}
 		
-		else if (num3 > num2 && num3 > num1) {
+		else if (num3 > num2 && num3 > number1) {
 			System.out.printf("\nThe third number %d is the largest of the three.\n", num3);
 		}
 		
-		else if (num1 == num2 && num1 > num3){
-			System.out.printf("\nThe first and second numbers %d are the largest of the three.\n", num1);
+		else if (number1 == num2 && number1 > num3){
+			System.out.printf("\nThe first and second numbers %d are the largest of the three.\n", number1);
 		}
 		
-		else if (num3 == num1 && num3 > num2){
+		else if (num3 == number1 && num3 > num2){
 			System.out.printf("\nThe first and third numbers %d are the largest of the three.\n", num3);
 		}
 		
-		else if (num3 == num2 && num3 > num1){
+		else if (num3 == num2 && num3 > number1){
 			System.out.printf("\nThe second and third numbers %d are the largest of the three.\n", num3);
 		}
 		
